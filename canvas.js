04a81da3ca66bfb0f1f758a9c628a7fdfe4dc641
Alan Wong <alan_wong888@yahoo.com>
2018-09-16 20:28:47 +0100

    Added resize event listener, Added init()

--- canvas.js
+++ canvas.js
@@ -4,111 +4,99 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-// c.fillRect(20, 100, 100, 100);
+var maxRadius = 40;
+var mouse = {
+  x: undefined,
+  y: undefined
+}
 
-// Arc Circle
-// for (var i = 0; i <10; i++) {
-//   var radius = 30
-//   var x = Math.random() * innerWidth;
-//   var y = Math.random() * innerHeight;
-//   var moveRightToLeft = Math.random() >= 0.5;
-//   var moveUpToDown = Math.random() >= 0.5;
-//   var speed = Math.random() * 10;
-//
-// }
+window.addEventListener('mousemove', (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+})
 
-
-// console.log(random_boolean);
+window.addEventListener('resize', () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  init();
+})
 
 class Circle {
-  constructor(context, radius, x, y, moveRightToLeft, moveUpToDown, speed, color) {
-    this.c = context;
+  constructor(radius, x, y, dx, dy, color) {
     this.radius = radius;
     this.x = x;
     this.y = y;
-    this.moveRightToLeft = moveRightToLeft;
-    this.moveUpToDown = moveUpToDown;
-    this.speed = speed;
+    this.dx = dx;
+    this.dy = dy;
+    this.color = color;
+    this.minRadius = radius;
+    this.maxRadius = maxRadius;
+
     this.draw = () => {
-      this.c.beginPath();
-      this.c.arc(x, y, radius, 0, Math.PI * 2, false);
-      this.c.strokeStyle = color;
-      this.c.stroke();
+      c.beginPath();
+      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      c.fillStyle = this.color;
+      c.fill();
+      // c.strokeStyle = this.color;
+
+      c.stroke();
+
     };
-    this.move = () => {
-      moveRightToLeft ? x += speed : x -= speed;
-      moveUpToDown ? y += speed : y -= speed;
 
-      if (x >= (innerWidth - radius) || x <= radius ) {
-        moveRightToLeft = !moveRightToLeft;
+    this.update = () => {
+
+      // Detects wall collision on x axis
+      if (this.x + this.radius > window.innerWidth || this.x -this.radius < 0) {
+        this.dx = -this.dx;
       }
 
-      if (y >= (innerHeight - radius) || y <= radius ) {
-        moveUpToDown = !moveUpToDown;
+      // Detects wall collision on y axis
+      if (this.y + this.radius > window.innerHeight || this.y - this.radius < 0) {
+        this.dy = -this.dy;
       }
+
+      this.x += this.dx;
+      this.y += this.dy;
+
+      // interactivity
+      if (mouse.x - this.x < 50 && mouse.x - this.x > -50 &&
+      mouse.y - this.y < 50 && mouse.y - this.y > -50 && this.radius < this.maxRadius) {
+        this.radius += 0.8;
+      } else if (this.radius > this.minRadius) {
+        this.radius -= 0.8;
+      }
+
       this.draw();
     };
   }
 }
 
 var circles = [];
+var init = () => {
+  circles = [];
+  for (var i = 0; i <100; i++) {
+    var radius = (Math.random() * 28) + 2;
+    var x = Math.random() * (innerWidth - 2 * radius) + radius;
+    var y = Math.random() * (innerHeight - 2 * radius) + radius;
+    var dx = Math.random() * -0.5;
+    var dy = Math.random() * -0.5;
+    var colorRed = Math.round(Math.random() * 255);
+    var colorGreen = Math.round(Math.random() * 255);
+    var colorBlue = Math.round(Math.random() * 255);
+    var colorAlpha = Math.random() * 255;
+    var color = `rgba(${colorRed}, ${colorGreen}, ${colorBlue}, ${colorAlpha})`;
 
-for (var i = 0; i <10; i++) {
-  var radius = 30
-  var x = Math.random() * innerWidth;
-  var y = Math.random() * innerHeight;
-  var moveRightToLeft = Math.random() >= 0.5;
-  var moveUpToDown = Math.random() >= 0.5;
-  var speed = Math.random() * 10;
-  var colorRed = Math.round(Math.random() * 255);
-  var colorGreen = Math.round(Math.random() * 255);
-  var colorBlue = Math.round(Math.random() * 255);
-  var colorAlpha = Math.random() * 255;
-  var color = `rgba(${colorRed}, ${colorGreen}, ${colorBlue}, ${colorAlpha})`;
-
-  let i = new Circle(c, radius, x, y, moveRightToLeft, moveUpToDown, speed, color);
-  // i.move();
-  i.draw();
-  circles.push(i);
+    circles.push(new Circle(radius, x, y, dx, dy, color));
+  }
 }
 
-
+init();
 var animate = () => {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
   circles.forEach((circle, index) => {
-    circle.move();
+    circle.update();
   })
-  // c.clearRect(0, 0, innerWidth, innerHeight);
-  // c.beginPath();
-  // c.arc(x, y, radius, 0, Math.PI * 2, false);
-  // c.strokeStyle = 'blue';
-  // c.stroke();
-  //
-  // moveRightToLeft ? x += speed : x -= speed;
-  // moveUpToDown ? y += speed : y -= speed;
-  //
-  // if (x >= (innerWidth - radius) || x <= radius ) {
-  //   moveRightToLeft = !moveRightToLeft;
-  // }
-  //
-  // if (y >= (innerHeight - radius) || y <= radius ) {
-  //   moveUpToDown = !moveUpToDown;
-  // }
 }
 
 animate();
-
-// for (var i = 0; i<10; i++) {
-//   var x = Math.random() * window.innerWidth;
-//   var y = Math.random() * window.innerHeight;
-//   var colorRed = Math.round(Math.random() * 255);
-//   var colorGreen = Math.round(Math.random() * 255);
-//   var colorBlue = Math.round(Math.random() * 255);
-//   var colorAlpha = Math.random() * 255;
-//
-//   c.beginPath();
-//   c.arc(x, y, 30, 0, Math.PI * 2, false);
-//   c.strokeStyle = `rgba(${colorRed}, ${colorGreen}, ${colorBlue}, ${colorAlpha})`;
-//   c.stroke();
-// }
